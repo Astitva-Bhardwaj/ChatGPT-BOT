b650323a382cda011f697dfb338cf75ea4786166
Astitva Bhardwaj <astitva8796@gmail.com>
2024-02-15 11:30:26 +0530

    Initial commit

---
+++ src/test/java/com/example/ChatgptBot/ChatGPTBOT/ChatGptBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChatgptBot.ChatGPTBOT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
